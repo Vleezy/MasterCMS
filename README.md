f85c6924b12933734bda7db3bb6c0d88bf72f381
Denzel Code <denzelgiraldocode@gmail.com>
2017-05-28 03:43:31 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,38 @@
+![Master logo](http://i.imgur.com/DajNToP.png)
+
+## What's MasterCMS?
+
+MasterCMS is an advanced management system of content for Retro Servers called "**The Retro Servers Framework**", written in PHP 7 with features like Advanced theme system *(yes, finally you can make you own theme with easiness)* unique functions like export, or import your template, select or even delete it... etc makes MasterCMS an amazing CMS for Retro Servers!
+
+## Features
+
+* HTML5
+* SASS
+* CSS3
+* JavaScript
+* JQuery
+* AJAX
+* PHP 7
+* MySQLi
+* MVC
+* POO
+* Sockets Connections
+* Templates System (Theme System)
+* Responsive (default theme)
+* Multi Language System
+* Advanced Routes System
+* Custom Controller
+* PIN System (HK and Client page for staffs)
+* Mailer System
+* Forgot Password System
+* Mail Changer System
+* Delete Account System
+* Housekeeping
+* News System
+* Rank Administration System (add, edit, give, delete rank)
+* User Administration System
+* Ban System
+* Badge Uploader
+* MPU Image's Uploader
+* LOG System
+* Ranks Clasifications (min, medium, max)
---
+++ master/index.php
@@ -0,0 +1,66 @@
+<?php 
+    
+    /**
+     *
+     *   MasterCMS
+     *
+     *   Copyright (c) 2017 MasterCMS
+     *
+     *   @author <Denzel Code>
+     *   -------------------------------------------------------------------------
+     *   Licensed under the Apache License, Version 2.0 (the "License");
+     *   you may not use this file except in compliance with the License.
+     *   You may obtain a copy of the License at
+     *
+     *       http://www.apache.org/licenses/LICENSE-2.0
+     *
+     *   Unless required by applicable law or agreed to in writing, software
+     *   distributed under the License is distributed on an "AS IS" BASIS,
+     *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     *   See the License for the specific language governing permissions and
+     *   limitations under the License.
+     *   -------------------------------------------------------------------------
+    */
+   
+	// Constants
+	error_reporting(0);
+	@ini_set('upload_max_filesize', '150M');
+	@ini_set('post_max_size', '150M');
+	define('DS', DIRECTORY_SEPARATOR);
+	define('DIRECTORY', realpath(dirname(__DIR__)) . DS);
+	define('MAIN_ROOT', __DIR__ . DS);
+	define('URL', $_SERVER['HTTP_HOST']);
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		define('TYPE_HTTP', 'https://');
+	} else {
+		define('TYPE_HTTP', 'http://');
+	}
+	date_default_timezone_set('Europe/Madrid');
+	
+	// Autoloader Require
+	try {
+		$need = '7.1.1';
+		if (version_compare(PHP_VERSION, $need) < 0) {
+        	throw new Exception('MasterCMS is not available for <strong>PHP ' . PHP_VERSION . '</strong><br>' . 'Please install PHP greater than <strong>PHP ' . $need . '</strong>');
+        } else {
+        	$rute = DIRECTORY . 'Code' . DS . 'Autoload.php';
+			if (is_readable($rute)) {
+				require_once $rute;
+			} else {
+				$rute = MAIN_ROOT . 'Code' . DS . 'Autoload.php';
+				if (is_readable($rute)) {
+					require_once $rute;
+				} else {
+					throw new Exception("Autoload class was not founded");
+				}
+			}
+        }
+	} catch (Exception $e) {
+		die($e->getMessage());
+	}
+
+	// Run Code
+	Autoload::run();
+	MasterCMS\Config\Router::run(new MasterCMS\Config\Request());
+	
+?>
